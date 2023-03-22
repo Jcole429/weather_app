e4d751e0d7e68ae2d4ad8ad86ad50bf72dc630aa
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-21 22:41:09 -0400

    Render index.html on get request for root and handle post request for root

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    getLocationInfo("Wharton", "NJ", "07885", (locationInfo) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+    getLocationInfo(req.body.cityName, (locationInfo) => {
         getWeatherInfo(locationInfo.lat, locationInfo.lon, "Imperial", (weatherInfo) => {
             const city = locationInfo.name;
+            const state = locationInfo.state;
             const temp = weatherInfo.main.temp;
             const weatherDescription = weatherInfo.weather[0].description;
             const icon = weatherInfo.weather[0].icon;
             const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
             res.write("<p> The weather is currently " + weatherDescription + "</p>");
-            res.write("<h1>The temperature in " + city + " is " + temp + " degrees.</h1>");
+            res.write("<h1>The temperature in " + city + ", " + state + " is " + temp + " degrees.</h1>");
             res.write("<img src=" + imageUrl + ">");
             res.send();
         });
@@ -25,9 +34,11 @@ app.listen(port, () => {
     console.log("Server is listening on port " + port);
 });
 
-function getLocationInfo(city, state, zip, fn) {
+function getLocationInfo(city, fn) {
     const baseUrl = "https://api.openweathermap.org/geo/1.0/direct?appid=" + apiKey;
-    const url = baseUrl + "&q=" + city + ", " + state + ", " + zip;
+    // const url = baseUrl + "&q=" + city + ", " + state + ", " + zip;
+    const url = baseUrl + "&q=" + city;
+
     https.get(url, (res) => {
         res.on("data", (data) => {
             const locationData = JSON.parse(data);
