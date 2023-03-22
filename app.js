f3270bcb981df34e6185b826ceaa5e49e76e26a6
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-21 22:12:25 -0400

    Add getLocationInfo()

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const https = require("https");
+
 const app = express();
 const port = 3000;
 
@@ -9,3 +11,16 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log("Server is listening on port " + port);
 });
+
+function getLocationInfo(city, state, zip, fn) {
+    const baseUrl = "https://api.openweathermap.org/geo/1.0/direct?appid=" + apiKey;
+    const url = baseUrl + "&q=" + city + ", " + state + ", " + zip;
+    https.get(url, (res) => {
+        res.on("data", (data) => {
+            const locationData = JSON.parse(data);
+            console.log("locationData[0]:");
+            console.log(locationData[0]);
+            fn(locationData[0]);
+        });
+    });
+}
